787566b2608a4e9c27186e6144aee525a78c74d1
Vladislav Vodenicharski <vodenicharski.vladislav@gmail.com>
2017-10-20 12:28:12 +0300

    Add categories model and tests

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @category = Category.create(name: "sports")
+  end
+
+  test "should get categories index"  do
+    get categories_url
+    assert_response :success
+  end
+
+  test "should get new"  do
+    get new_category_url
+    assert_response :success
+  end
+
+  test "should get show"  do
+    get category_url(@category)
+    assert_response :success
+  end
+end
---
+++ test/integration/create_categories_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  test "get new category form and create category" do
+    get "/categories/new"
+    assert_response :success
+
+    assert_difference 'Category.count', 1 do
+      post "/categories",
+            params: {category: {name: "sports"}}
+    end
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_match "sports", response.body
+  end
+
+  test "invalid category submission results in failure"  do
+    get "/categories/new"
+    assert_response :success
+
+    assert_no_difference 'Category.count' do
+      post "/categories",
+            params: {category: {name: " "}}
+    end
+    assert_template 'categories/new'
+    assert_select "div.card-header"
+    assert_select "div.card-body"
+  end
+end
